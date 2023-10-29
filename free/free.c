ca5499b89c7448d39f0bdd5aa5ec42e1f93e5683
Baran Kılıç <96842857+TheDeveloperBaran@users.noreply.github.com>
2023-10-29 21:31:50 +0300

    free

---
+++ free/free.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilsahin <ilsahin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/28 14:07:29 by ilsahin           #+#    #+#             */
+/*   Updated: 2023/10/28 14:07:30 by ilsahin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	free_array(char **arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr[i])
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
+}
+
+void	free_process(void)
+{
+	t_process	*tmp;
+	t_process	*process;
+
+	process = g_ms.process;
+	if (!process)
+		return ;
+	while (process)
+	{
+		free_array(process->execute);
+		free_array(process->redirects);
+		tmp = process;
+		process = process->next;
+		free(tmp);
+	}
+}
---
+++ free/free_token.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_token.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilsahin <ilsahin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/28 14:07:43 by ilsahin           #+#    #+#             */
+/*   Updated: 2023/10/28 14:07:44 by ilsahin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	free_token(void)
+{
+	t_token		*tmp;
+	t_token		*token;
+
+	token = g_ms.token;
+	while (token)
+	{
+		free(token->str);
+		token = token->next;
+	}
+	token = g_ms.token;
+	while (token)
+	{
+		tmp = token;
+		token = token->next;
+		free(tmp);
+	}
+}
