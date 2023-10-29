cd57270b4c517e558d78cd7814df45527627bd29
Baran Kılıç <96842857+TheDeveloperBaran@users.noreply.github.com>
2023-10-29 21:33:18 +0300

    cmd

---
+++ cmd/close.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   close.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilsahin <ilsahin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/28 14:06:43 by ilsahin           #+#    #+#             */
+/*   Updated: 2023/10/28 14:06:45 by ilsahin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	close_heredoc_fd(t_process *process)
+{
+	if (contain_heredoc(process) && process->heredoc_fd[0] > 2)
+		close(process->heredoc_fd[0]);
+}
+
+void	close_all_fd(void)
+{
+	t_process	*process;
+
+	process = g_ms.process;
+	while (process)
+	{
+		close_heredoc_fd(process);
+		if (process->fd[0] > 2)
+			close(process->fd[0]);
+		if (process->fd[1] > 2)
+			close(process->fd[1]);
+		process = process->next;
+	}
+}
