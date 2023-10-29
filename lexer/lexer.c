7eca3aada1bc26df459bbd22fe282d7ca88f7fa2
Baran Kılıç <96842857+TheDeveloperBaran@users.noreply.github.com>
2023-10-29 21:31:00 +0300

    lexer

---
+++ lexer/lexer.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilsahin <ilsahin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/28 14:09:16 by ilsahin           #+#    #+#             */
+/*   Updated: 2023/10/28 15:56:51 by ilsahin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	free_lexer(void)
+{
+	free_token();
+	free_process();
+}
+
+int	append_arguments(t_token **token, t_process *process)
+{
+	char		*data;
+
+	if ((*token)->type == STRING)
+	{
+		data = clean_quote((*token)->str);
+		process->execute = push_array(process->execute, data);
+	}
+	else
+	{
+		data = clean_quote((*token)->str);
+		process->redirects = push_array(process->redirects, data);
+		*token = (*token)->next;
+		if (!(*token) || (*token)->type != STRING)
+		{
+			if (!(*token))
+				token_err(0);
+			else
+				token_err((*token)->type);
+			free_lexer();
+			return (FALSE);
+		}
+		data = clean_quote((*token)->str);
+		process->redirects = push_array(process->redirects, data);
+	}
+	return (TRUE);
+}
+
+int	while_loop(t_token *token, t_process *process)
+{
+	while (token)
+	{
+		if (token->type == PIPE && token->prev == NULL)
+		{
+			token_err(PIPE);
+			break ;
+		}
+		if (token->type == PIPE || token->prev == NULL)
+		{
+			if (token->type == PIPE)
+				token = token->next;
+			process = init_process();
+			process_addback(&g_ms.process, process);
+			g_ms.process_count++;
+		}
+		if (!token)
+		{
+			token_err(PIPE);
+			break ;
+		}
+		if (!append_arguments(&token, process))
+			return (FALSE);
+		token = token->next;
+	}
+	return (TRUE);
+}
+
+int	lexer(void)
+{
+	t_token		*token;
+	t_process	*process;
+	int			err;
+
+	process = NULL;
+	token = g_ms.token;
+	err = while_loop(token, process);
+	if (err == FALSE)
+		return (FALSE);
+	free_token();
+	return (TRUE);
+}
