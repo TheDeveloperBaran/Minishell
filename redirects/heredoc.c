d0a22a9c4ff16932dc33a0afd6ed3f72fa38476d
Baran Kılıç <96842857+TheDeveloperBaran@users.noreply.github.com>
2023-10-29 21:32:22 +0300

    redirect

---
+++ redirects/heredoc.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilsahin <ilsahin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/28 14:05:11 by ilsahin           #+#    #+#             */
+/*   Updated: 2023/10/28 14:08:12 by ilsahin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	close_heredoc(int sig)
+{
+	(void)sig;
+	g_ms.ignore = TRUE;
+	ioctl(STDIN_FILENO, TIOCSTI, "\n");
+}
+
+void	heredoc(int *fd, char *endline)
+{
+	char		*input;
+
+	if (pipe(fd) < 0)
+		return (perror("minishell"));
+	while (1)
+	{
+		signal(SIGINT, &close_heredoc);
+		input = readline("> ");
+		if (!input || ft_strcmp(input, endline) || g_ms.ignore)
+		{
+			free(input);
+			break ;
+		}
+		write(fd[1], input, ft_strlen(input));
+		write(fd[1], "\n", 1);
+		free(input);
+	}
+	close(fd[1]);
+}
